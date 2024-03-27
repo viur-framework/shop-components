adce59b9fd61591f1478af2bbf57fc783acc2ebb
KadirBalku <Kadir.Balku@gmail.com>
2024-03-27 22:54:21 +0100

    initial commit

--- source/src/components/order/category/CategoryList.vue
+++ source/src/components/order/category/CategoryList.vue
@@ -1,3 +1,10 @@
 <template>
   <div>HALLO WELT</div>
+  <div v-for="item in list" :key="item">{{ item }}</div>
 </template>
+
+<script setup>
+const props = defineProps({
+  list: { type: Array, required: true },
+});
+</script>
