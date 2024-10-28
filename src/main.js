16cce1ff86248a835d2f178d3fd9d3b195a28772
Kadir Balku <KadirBalku@gmail.com>
2024-10-28 15:50:08 +0100

    fixes

--- src/main.js
+++ src/main.js
@@ -19,34 +19,31 @@ export { useCartStore, createRouterInstance };
 
 // export all main components
 export { default as ShopOrderStepper } from "./components/ShopOrderStepper.vue";
+export { default as ShopUserData } from "./components/ShopUserData.vue";
+
 export { default as CartView } from "./components/cart/CartView.vue";
 export { default as ExampleUsage } from "./components/order/process/ExampleUsage.vue";
 export { default as ConfirmView } from "./components/ShopOrderConfirm.vue";
 export { default as OrderComplete } from "./components/ShopOrderComplete.vue";
-export { default as UserInformation } from "./components/order/information/UserInformation.vue";
-export { default as UserInfoMulti } from "./components/order/information/UserInfoMulti.vue";
+// export { default as UserInformation } from "./components/order/information/UserInformation.vue";
+// export { default as UserInfoMulti } from "./components/order/information/UserInfoMulti.vue";
 export { default as SelectPaymentProvider } from "./components/order/process/SelectPaymentProvider.vue";
 
 const pinia = createPinia();
-const i18n = createI18n({
-  locale: "de",
-  fallbackLocale: "en",
-  messages: {
-    en: { ...en_translations },
-    de: { ...de_translations },
-  },
-});
+// const i18n = createI18n({
+//   locale: "de",
+//   fallbackLocale: "en",
+//   messages: {
+//     en: { ...en_translations },
+//     de: { ...de_translations },
+//   },
+// });
 
 // Create a plugin to install all components
 const ViurShopComponents = {
   install(app) {
-    app.component("CartNode", CartNode);
-    app.component("CartLeaf", CartLeaf);
-    app.use(PrimeVue, {
-      unstyled: true,
-    });
     app.use(pinia);
-    app.use(i18n);
+    // app.use(i18n);
   },
 };
 
