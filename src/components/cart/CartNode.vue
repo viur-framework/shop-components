1bd2750f3b27e6483751310c17715f9dd799cef0
tim <tk@mausbrand.de>
2024-11-08 14:28:20 +0100

    Template for cart groups and editable cart headers

--- src/components/cart/CartNode.vue
+++ src/components/cart/CartNode.vue
@@ -1,8 +1,26 @@
 <template>
-  <sl-details v-if="node.children" :summary="node.name">
-    <slot></slot>
-  </sl-details>
-
+  <div class="viur-shop-sub-cart-wrap" v-if="node.children" :summary="node.name">
+    <div class="viur-shop-sub-cart-head">
+      <div class="viur-shop-sub-cart-preview">
+        <!-- hier bis zu 4 bilder der nodes einfügen -->
+        <img src="https://serene-allen-537100.netlify.app/assets/examples/carousel/mountains.jpg">
+        <img src="https://serene-allen-537100.netlify.app/assets/examples/carousel/mountains.jpg">
+        <img src="https://serene-allen-537100.netlify.app/assets/examples/carousel/mountains.jpg">
+        <img src="https://serene-allen-537100.netlify.app/assets/examples/carousel/mountains.jpg">
+      </div>
+      <div class="viur-shop-sub-cart-head-inner">
+        <h3 class="viur-shop-sub-cart-headline headline">{{ node.name }}</h3>
+        <div>Artikel: 12</div>
+        <div>Gesamtpreis: 12.345,00 €</div>
+      </div>
+      <sl-button size="large" class="viur-shop-sub-cart-btn">
+        <sl-icon name="chevron-drown"></sl-icon>
+      </sl-button>
+    </div>
+    <div class="viur-shop-sub-cart-list">
+      <slot></slot>
+    </div>
+  </div>
   <h1 class="viur-shop-cart-headline" v-else>
     {{ node.name }}
   </h1>
@@ -21,5 +39,78 @@ const state = reactive({});
 <style scoped>
 .viur-shop-cart-headline {
   font-size: 2rem;
+}
+
+.viur-shop-sub-cart-head{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: var(--sl-spacing-large);
+  margin-bottom: var(--sl-spacing-x-large);
+}
+
+.viur-shop-sub-cart-head-inner{
+  flex: 1 1 auto;
+  width: 1px;
+
+}
+
+.viur-shop-sub-cart-headline{
+  margin-bottom: var(--sl-spacing-2x-small);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.viur-shop-sub-cart-preview{
+  aspect-ratio: 1;
+  overflow: hidden;
+  min-width: 5em;
+  width: 5.5em;
+  display: grid;
+  grid-template-columns: 50% 50%;
+
+  img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+
+    &:nth-child(1){
+      grid-column: span 2;
+
+      &:has(+ img){
+        grid-column: span 1;
+        aspect-ratio: 1;
+      }
+    }
+
+    &:nth-child(3){
+      grid-column: span 2;
+      aspect-ratio: 2/1;
+
+      &:has(+ img){
+        grid-column: span 1;
+        aspect-ratio: 1;
+      }
+    }
+  }
+}
+
+.viur-shop-sub-cart-btn{
+  margin-left: auto;
+
+  &::part(base){
+    width: var(--sl-input-height-large);
+  }
+}
+
+.viur-shop-sub-cart-wrap{
+  margin-bottom: var(--sl-spacing-2x-large);
+  border-bottom: 1px solid var(--sl-color-neutral-400);
+
+  & .viur-shop-sub-cart-wrap{
+    margin-left: var(--sl-spacing-large);
+  }
 }
 </style>
--- src/components/cart/CartTree.vue
+++ src/components/cart/CartTree.vue
@@ -1,4 +1,19 @@
 <template>
+  <div class="viur-shop-tree-head">
+    <sl-input class="viur-shop-tree-headline headline">
+      <sl-icon name="pencil" slot="suffix"></sl-icon>
+    </sl-input>
+    <sl-dropdown class="viur-shop-tree-dropdown">
+      <sl-icon name="dots" slot="trigger"></sl-icon>
+      <sl-menu class="viur-shop-tree-menu">
+        <sl-menu-item class="viur-shop-tree-menu-item">Warenkorb kopieren</sl-menu-item>
+        <sl-menu-item class="viur-shop-tree-menu-item">Warenkorb löschen</sl-menu-item>
+      </sl-menu>
+    </sl-dropdown>
+  </div>
+  <sl-input size="medium" class="viur-shop-tree-descr">
+    <sl-icon name="pencil" slot="suffix"></sl-icon>
+  </sl-input>
   <CartTreeWrapper :tree="state.tree" @update:modelValue="handleChange">
   </CartTreeWrapper>
 </template>
@@ -52,3 +67,94 @@ function handleChange(e) {
 //  }
 //  );
 </script>
+
+<style scoped>
+
+  .viur-shop-tree-head{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: var(--sl-spacing-medium);
+    margin-bottom: var(--sl-spacing-small);
+  }
+
+  .viur-shop-tree-headline{
+    width: 100%;
+    border-bottom: 1px solid transparent;
+    transition: all ease .3s;
+
+    &:hover{
+      border-bottom: 1px solid var(--ignt-color-text-default);
+    }
+
+    &::part(base){
+      border: none;
+    }
+
+    &::part(prefix){
+      display: none;
+    }
+
+    &::part(suffix){
+      pointer-events: none;
+      position: absolute;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      font-size: 1rem;
+    }
+
+    &::part(input){
+      padding-right: 0;
+      padding-left: 0;
+      font-size: 1.8em;
+      color: var(--ignt-color-primary);
+    }
+  }
+
+  .viur-shop-tree-descr{
+    width: 100%;
+    border-bottom: 1px solid transparent;
+    transition: all ease .3s;
+    margin-bottom: var(--sl-spacing-x-large);
+
+
+    &:hover{
+      border-bottom: 1px solid var(--ignt-color-text-default);
+    }
+
+    &::part(base){
+      border: none;
+    }
+
+    &::part(prefix){
+      display: none;
+    }
+
+    &::part(suffix){
+      pointer-events: none;
+      position: absolute;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      font-size: 1rem;
+    }
+
+    &::part(input){
+      padding-right: 0;
+      padding-left: 0;
+      font-size: 1rem;
+      color: var(--ignt-color-text-default);
+    }
+  }
+
+  .viur-shop-tree-dropdown{
+    &::part(trigger){
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 1.8em;
+      height: 1.8em;;
+    }
+  }
+</style>
