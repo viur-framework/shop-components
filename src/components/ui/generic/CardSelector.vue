98cda023f1125a52444ceb3d8fb1a554ab56a45e
Andreas H. Kelch <32266000+akelch@users.noreply.github.com>
2024-11-18 12:48:58 +0100

    feat: added standard Cardselector for ShippingMethod and Payment later

---
+++ src/components/ui/generic/CardSelector.vue
@@ -0,0 +1,51 @@
+<template>
+    <template v-for="(option,i) in options">
+        <sl-card selectable @sl-change="changeSelection(i)" :selected="i===state.currentSelection" horizontal>
+            <slot :option="option" :index="i">
+                {{i}}: {{ option }} 
+            </slot>
+        </sl-card>
+    </template>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+
+const selection = defineModel("selection")
+
+const emits = defineEmits()
+
+const props = defineProps({
+    options:{
+        type: Array,
+        default:[]
+    },
+})
+
+const state = reactive({
+    currentSelection:0
+})
+
+function changeSelection(i){
+    if (state.currentSelection === i){
+        state.currentSelection = null
+        selection.value = null
+    }else{
+        state.currentSelection = i
+        selection.value = props.options[i]
+    }   
+}
+
+
+</script>
+
+<style scoped>
+sl-card{
+    width: 100%;
+    &[selected]::part(base){
+        border:1px solid var(--sl-color-primary-500);
+        box-shadow: 0 0 0 var(--sl-focus-ring-width) var(--sl-input-focus-ring-color);
+    }
+}
+
+</style>
