03c85d8c910f07c25d5f19cf46cf8b5ed752ab7a
Kadir Balku <KadirBalku@gmail.com>
2024-11-22 08:05:06 +0100

    chore: cleanup main.js

--- src/main.js
+++ src/main.js
@@ -7,31 +7,24 @@ import { useCartStore } from "./stores/cart";
 import { useOrderStore } from "./stores/order";
 import { useShippingStore } from "./stores/shipping";
 
-import CartNode from "./components/cart/CartNode.vue";
-import CartLeaf from "./components/cart/CartLeaf.vue";
-// import { createApp } from "vue";
-// import bone from "@viur/vue-utils/bones/edit/bone.vue";
-// import Wrapper_nested from "@viur/vue-utils/bones/edit/wrapper_nested.vue";
-
-export { useCartStore };
-export { useOrderStore, useShippingStore };
+export { useCartStore, useOrderStore, useShippingStore };
 
 // export all main components
-export { default as ShopOrderStepper } from "./components/ShopOrderStepper.vue";
-export { default as ShopUserData } from "./components/ShopUserData.vue";
-
-export { default as CartView } from "./components/cart/CartView.vue";
-export { default as ExampleUsage } from "./components/ExampleUsage.vue";
-export { default as ShopOrderConfirm } from "./components/ShopOrderConfirm.vue";
-export { default as OrderComplete } from "./components/ShopOrderComplete.vue";
-export { default as ShopShippingMethod } from "./components/ShopShippingMethod.vue";
-export { default as ShopPaymentProvider } from "./components/ShopPaymentProvider.vue";
-// export { default as UserInformation } from "./components/order/information/UserInformation.vue";
-// export { default as UserInfoMulti } from "./components/order/information/UserInfoMulti.vue";
 export { default as ShopCart } from "./components/ShopCart.vue";
 export { default as ShopOrderComplete } from "./components/ShopOrderComplete.vue";
+export { default as ShopOrderConfirm } from "./components/ShopOrderConfirm.vue";
+export { default as ShopOrderStepper } from "./components/ShopOrderStepper.vue";
+export { default as ShopPaymentProvider } from "./components/ShopPaymentProvider.vue";
+export { default as ShopShippingMethod } from "./components/ShopShippingMethod.vue";
+export { default as ShopUserData } from "./components/ShopUserData.vue";
+export { default as ShopSummary } from "./components/ShopSummary.vue";
 
+// deleted after docs are written
+export { default as ExampleUsage } from "./components/ExampleUsage.vue";
+
+// Plugin registration
 const pinia = createPinia();
+
 const i18n = createI18n({
   locale: "de",
   fallbackLocale: "en",
@@ -41,7 +34,6 @@ const i18n = createI18n({
   },
 });
 
-// Create a plugin to install all components
 const ViurShopComponents = {
   install(app) {
     app.use(pinia);
