1bac4f25f8c17e76ad4b9b85a45469884c5ba896
KadirBalku <kb@mausbrand.de>
2025-02-07 21:35:19 +0100

    feat: use additional data for order requests which are not defined by default

--- src/composables/order.js
+++ src/composables/order.js
@@ -36,39 +36,43 @@ export function useOrder() {
     }
 
     function addOrUpdateOrder({
-        payment_provider,
-        billing_address_key,
-        customer_key,
-        state_ordered,
-        state_paid,
-        state_rts
-    }={}){
-        state.isUpdating=true
-        let url = shopStore.state.shopApiUrl+"/order_add"
-        let data = {
-            payment_provider:payment_provider?payment_provider:shopStore.state.order?.['payment_provider'],
-            billing_address_key:billing_address_key?billing_address_key:shopStore.state.order?.['billing_address_key']?.['dest']?.['key'],
-            customer_key:customer_key?customer_key:shopStore.state.order?.['customer_key']?.['dest']?.['key'],
-            state_ordered:state_ordered?state_ordered:shopStore.state.order?.['state_ordered'],
-            state_paid:state_paid?state_paid:shopStore.state.order?.['state_paid'],
-            state_rts:state_rts?state_rts:shopStore.state.order?.['state_rts']
-        }
-        if (shopStore.state.orderKey){
-            url = shopStore.state.shopApiUrl+"/order_update"
-            data["order_key"] = shopStore.state.orderKey
+      payment_provider,
+      billing_address_key,
+      customer_key,
+      state_ordered,
+      state_paid,
+      state_rts,
+      //TODO: ...additionalData Über alle requests in den components für customized Projekte
+      ...additionalData  // abfangen von nicht definierten Feldern
+  }={}){
+      state.isUpdating=true
+      let url = shopStore.state.shopApiUrl+"/order_add"
+      let data = {
+          payment_provider:payment_provider?payment_provider:shopStore.state.order?.['payment_provider'],
+          billing_address_key:billing_address_key?billing_address_key:shopStore.state.order?.['billing_address_key']?.['dest']?.['key'],
+          customer_key:customer_key?customer_key:shopStore.state.order?.['customer_key']?.['dest']?.['key'],
+          state_ordered:state_ordered?state_ordered:shopStore.state.order?.['state_ordered'],
+          state_paid:state_paid?state_paid:shopStore.state.order?.['state_paid'],
+          state_rts:state_rts?state_rts:shopStore.state.order?.['state_rts'],
+          ...additionalData
+      }
+      if (shopStore.state.orderKey){
+          url = shopStore.state.shopApiUrl+"/order_update"
+          data["order_key"] = shopStore.state.orderKey
 
-        }else{
-            data["cart_key"] = shopStore.state.cartRoot['key']
-        }
-        return Request.post(url,{dataObj:removeUndefinedValues(data)}).then(async(resp)=>{
-            let data = await resp.json()
-            updateOrderState(data['key'], data)
-            state.isUpdating=false
-        }).then(async ( resp)=>{
-            fetchOrder(shopStore.state.orderKey)
-            return resp
-        })
-    }
+      }else{
+          data["cart_key"] = shopStore.state.cartRoot['key']
+      }
+      return Request.post(url,{dataObj:removeUndefinedValues(data)}).then(async(resp)=>{
+          let data = await resp.json()
+          updateOrderState(data['key'], data)
+          state.isUpdating=false
+      }).then(async ( resp)=>{
+          fetchOrder(shopStore.state.orderKey)
+          return resp
+      })
+  }
+
 
     return {
         state,
