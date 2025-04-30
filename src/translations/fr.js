2d8d3c180052d686079223906b4cbc8e1a0a58ec
Sven Eberth <mail@sveneberth.de>
2025-04-30 23:52:00 +0200

    fix: translation variable

--- src/translations/fr.js
+++ src/translations/fr.js
@@ -39,7 +39,7 @@ export default {
       'summary_discount': 'Vous économisez sur votre achat',
       'summary_checkout': 'Acheter cet article',
       'summary_total': 'Montant total',
-      'summary_vat': 'TVA incluse ({vat_percentage})',
+      'summary_vat': 'TVA incluse ({percentage})',
       'discount_not_found': 'Ce code promo n’existe pas.',
       'discount_not_active': 'Ce code promo n’est pas actif.',
       'discount_not_available': 'Ce code promo n’est pas valable pour ce pays.',
