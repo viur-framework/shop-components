046665c5fcd88cf25802320df63a76e2c1d8d233
Sven Eberth <mail@sveneberth.de>
2025-05-20 17:11:39 +0200

    fix: use shipping as store and reload available shipping options after address has been changed

--- src/composables/shipping.js
+++ src/composables/shipping.js
@@ -1,40 +1,46 @@
-import {reactive} from 'vue'
-import {Request} from '@viur/vue-utils'
-import { removeUndefinedValues} from '../utils'
-import { useI18n } from 'vue-i18n'
-import { useViurShopStore } from '../shop'
+import {Request} from '@viur/vue-utils';
+import {defineStore} from 'pinia';
+import {reactive} from 'vue';
+import {useI18n} from 'vue-i18n';
+import {useViurShopStore} from '../shop';
 
-export function useShipping() {
-    const i18n = useI18n()
-    const shopStore = useViurShopStore()
-    const defaultErrorMessage = i18n.t('viur.shop.error_message')
-    const state = reactive({
-        isLoading:false,
-        isUpdating:false,
-        hasError:false,
-        errorMessage: defaultErrorMessage,
-        shippingData:[]
-    })
+/**
+ * Store that holds available shipping options
+ */
+export const useShipping = defineStore('useShippingStore', () => {
+  const i18n = useI18n();
+  const shopStore = useViurShopStore();
+  const defaultErrorMessage = i18n.t('viur.shop.error_message');
+  const state = reactive({
+    isLoading: false,
+    isUpdating: false,
+    hasError: false,
+    errorMessage: defaultErrorMessage,
+    shippingData: [],
+  });
 
-    function fetchShippingData() {
-        state.isLoading = true
-        Request.get(`${shopStore.state.shopApiUrl}/shipping_list`,{dataObj:{
-            cart_key: shopStore.state.cartRoot['key']
-        }}).then(async (resp)=>{
-            let data = await resp.json()
-            state.shippingData = data
-            if (state.shippingData.length===0){
-                state.hasError = true
-                state.errorMessage = i18n.t("shop.no_valid_shipping_found")
-            }
-            state.isLoading = false
-        })
-    }
+  /**
+   * fetch available shipping options from API
+   */
+  function fetchShippingData() {
+    state.isLoading = true;
+    return Request.get(`${shopStore.state.shopApiUrl}/shipping_list`, {
+      dataObj: {
+        cart_key: shopStore.state.cartRoot['key'],
+      },
+    }).then(async (resp) => {
+      state.shippingData = await resp.json();
+      if (state.shippingData.length === 0) {
+        state.hasError = true;
+        state.errorMessage = i18n.t('shop.no_valid_shipping_found');
+      }
+      state.isLoading = false;
+    });
+  }
 
 
-    return {
-        state,
-        fetchShippingData
-
-    }
-}
+  return {
+    state,
+    fetchShippingData,
+  };
+});
